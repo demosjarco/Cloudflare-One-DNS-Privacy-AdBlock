9f112408d45def074c6d065e7fea9abb6d3eea88
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2026-09-12 15:33:54 -0700

    ESLint

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig({
+	// config with just ignores is the replacement for `.eslintignore`
+	ignores: ['dist/**', 'worker-configuration.d.ts'],
+	extends: [eslint.configs.recommended, tseslint.configs.recommendedTypeChecked, tseslint.configs.stylisticTypeChecked, eslintConfigPrettier],
+	plugins: {
+		'@typescript-eslint': tseslint.plugin,
+	},
+	languageOptions: {
+		parser: tseslint.parser,
+		parserOptions: {
+			ecmaVersion: 'latest',
+			jsDocParsingMode: 'type-info',
+			lib: ['esnext'],
+			projectService: {
+				allowDefaultProject: ['eslint.config.js'],
+				defaultProject: 'tsconfig.json',
+			},
+			tsconfigRootDir: import.meta.dirname,
+			ecmaFeatures: {
+				jsx: true,
+			},
+		},
+	},
+	rules: {
+		'@typescript-eslint/consistent-type-imports': ['error', { disallowTypeAnnotations: false }],
+		'@typescript-eslint/no-empty-object-type': ['error', { allowInterfaces: 'with-single-extends' }],
+		'@typescript-eslint/no-confusing-void-expression': ['error', { ignoreArrowShorthand: true, ignoreVoidOperator: true }],
+		'@typescript-eslint/no-explicit-any': ['warn', { ignoreRestArgs: true }],
+		'@typescript-eslint/restrict-plus-operands': 'warn',
+		'no-async-promise-executor': 'off',
+		'@typescript-eslint/require-await': 'off',
+		'@typescript-eslint/no-misused-promises': 'off',
+	},
+});
